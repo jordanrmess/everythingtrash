f55e26aa44118fedfb4d445bcbdb2670101b7b97
Jordan Mess <jordanmess@Jordans-MacBook-Pro.local>
2026-07-11 12:14:47 -0400

    add paper sound effect

--- js/trash.js
+++ js/trash.js
@@ -11,10 +11,15 @@ export class Trash {
   handleClick(e) {
     this.count = e.shiftKey ? 1 : Math.min(this.count + 1, 5);
     this.element.src = `./${this.type}/${this.count}.png`;
-
-    if (this.type === "cans") {
-      const audio = new Audio("./cans/crush.mp3");
-      audio.play().catch(() => {});
+    console.log(this.count);
+    if (this.count != 5) {
+      if (this.type === "cans") {
+        const audio = new Audio("./cans/crush.mp3");
+        audio.play().catch(() => {});
+      } else if (this.type === "cups") {
+        const audio = new Audio("./cups/crush.mp3");
+        audio.play().catch(() => {});
+      }
     }
   }
 }
